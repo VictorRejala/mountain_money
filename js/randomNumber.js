a1a0897cbb9010fde635cd611192e6f30fe54d35
Rejala <victorrejala@gmail.com>
2022-11-01 23:21:50 -0300

    second changes

--- js/randomNumber.js
+++ js/randomNumber.js
@@ -3,8 +3,9 @@ var allTry = 7;
 var actualTry = 0;
 var infoGame = document.getElementById("infoGame");
 var infoTry = document.getElementById("infoTry");
-
 var win = false;
+const musicWinner = new Audio("music/public_congratulations.mp3");
+const musicLoser = new Audio("music/loser.mp3");
 
 function checkNumber() {
 
@@ -13,10 +14,10 @@ function checkNumber() {
     if(inputNumber == randomNumber) {
 
         infoGame.innerHTML = "Você ganhou! O numero pensado realmente é " + randomNumber + "<br> Clique em Reiniciar para jogar de novo"; 
-        infoGame.style.color="green"
+        infoGame.style.color="green";
         win = true;
-        const musicWin = new Audio("music/public_congratulations.mp3");
-        musicWin.play();
+        musicWinner.volume="0.1";
+        musicWinner.play();
     } else if (inputNumber > randomNumber && inputNumber < 101) {
 
         infoGame.innerHTML = "O seu chute foi maior que o esperado";
@@ -33,10 +34,10 @@ function checkNumber() {
         infoGame.innerHTML="Você perdeu! O numero pensado era " + randomNumber + "<br> Clique em Reiniciar para jogar de novo";
         infoGame.style.color="red";
         actualTry = 7;
-        const musicLoser = new Audio("music/loser.mp3");
+        musicLoser.volume="0.1";
         musicLoser.play();
     }        
-};
+}
 
 function attInfo() {
     
@@ -47,7 +48,35 @@ function attInfo() {
     }
 }
 
-var buttonPlay = document.querySelector("#play");
+function playRandomNumber() {
+
+    event.preventDefault();
+
+    if(win) {
+        infoGame.innerHTML = "Você ganhou! O numero pensado realmente é " + randomNumber + "<br> Clique em Reiniciar para jogar de novo"; 
+        infoGame.style.color="green"
+    } else {
+        checkNumber();
+    }
+    attInfo();    
+    inputNumber = document.getElementById("number");
+    inputNumber.value="";
+}
+
+function restartGame() {
+
+    actualTry = 0;
+    win = false;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    infoTry.innerHTML="Você tem 7 chances";
+    infoGame.innerHTML="Boa sorte";
+    infoGame.style.color="white";
+    musicLoser.volume="0";
+    musicWinner.volume="0";
+}
+
+
+/*var buttonPlay = document.querySelector("#play");
 
 buttonPlay.addEventListener("click", function(event){
 
@@ -71,3 +100,5 @@ buttonRestart.addEventListener("click", function(event){
     event.preventDefault();
     location.reload();
 })
+
+var testNumber = 0;*/
